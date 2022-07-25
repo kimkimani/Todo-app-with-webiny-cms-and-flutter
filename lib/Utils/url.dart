f99450029096168e81e266a4bc3f797d5f92c2d4
kimkimani <kimkimani016@gmail.com>
2022-07-25 14:03:17 +0300

    Delete url.dart

--- lib/Utils/url.dart
+++
@@ -1,4 +0,0 @@
-const String endpointUrl =
-    "https://d3aafxj8o27k36.cloudfront.net/cms/read/en-US";
-const String mutationEndpointUrl =
-    "https://d3aafxj8o27k36.cloudfront.net/cms/manage/en-US";
