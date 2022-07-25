c15c0e874915868e1696c03f34613b40c98e239c
kimkimani <kimkimani016@gmail.com>
2022-07-25 13:57:43 +0300

    webiny-flutter

---
+++ lib/Utils/url.dart
@@ -0,0 +1,4 @@
+const String endpointUrl =
+    "https://d3aafxj8o27k36.cloudfront.net/cms/read/en-US";
+const String mutationEndpointUrl =
+    "https://d3aafxj8o27k36.cloudfront.net/cms/manage/en-US";
